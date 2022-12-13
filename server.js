6fcabd1a285175ac9170c9997d807798d225a7cc
shellawa <awasymmetry@gmail.com>
2022-12-14 00:05:51 +0700

    Testing modified fonts with Japanese characters from NotoSansJP

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const {generate} = require("text-to-image")
+const { generate } = require("text-to-image")
 const dataUriToBuffer = require("data-uri-to-buffer")
 const app = express()
 const port = process.env.PORT || 3000
@@ -9,9 +9,7 @@ app.get("/imgen", async (req, res) => {
   if (!text) return res.status(400).send("No text provided")
   console.log("requesting " + text)
   const dataUri = await generate(text, {
-    // fontFamily: "Times New Roman,Georgia,serif",
-    fontPath: "./assets/fonts/Merriweather.ttf",
-    fontFamily: "Merriweather,Noto Sans JP, sans-serif",
+    fontPath: "./assets/fonts/Merriweather-JP.ttf",
     fontSize: 18,
     margin: 15,
     maxWidth: 414,
